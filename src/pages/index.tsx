1240ade6676126fd642b3631b76d2942d038fc5e
WY <kim.manon2@gmail.com>
2024-01-22 00:32:13 +0900

    kakao map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,35 @@
-import Link from 'next/link';
-import React from 'react';
+/* global kakao */
+import Script from 'next/script';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 const Home = () => {
+  const loadKakaoMap = () => {
+    //kakao map 로드
+    window.kakao.maps.load(() => {
+      const mapContainer = document.getElementById('map');
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      new window.kakao.maps.Map(mapContainer, mapOption);
+    });
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-      <ul>
-        <li>
-          <Link href={'/stores'}>맛집 목록</Link>
-        </li>
-        <li>
-          <Link href={'/stores/new'}>맛집 생성</Link>
-        </li>
-        <li>
-          <Link href={'/stores/1'}>맛집 상세 페이지</Link>
-        </li>
-        <li>
-          <Link href={'/stores/1/edit'}>맛집 수정 페이지</Link>
-        </li>
-        <li>
-          <Link href={'/users/login'}>로그인 페이지</Link>
-        </li>
-        <li>
-          <Link href={'/users/likes'}>맛집 수정 페이지</Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <Script
+        strategy='afterInteractive'
+        type='text/javascript'
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+        onReady={loadKakaoMap}
+      />
+      <div id='map' className='w-full h-screen'></div>
+    </>
   );
 };
 
