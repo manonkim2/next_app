a422d413dae5459e3e6dc3240c1a75a68a1751f3
Manon Kim <kim.manon2@gmail.com>
2024-10-28 21:24:01 +0900

    [feat] product upload

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,10 +1,70 @@
 'use server'
 
-export const uploadProduct = async (formData: FormData) => {
+import { z } from 'zod'
+import fs from 'fs/promises'
+import getSession from '@/lib/session'
+import db from '@/lib/db'
+import { redirect } from 'next/navigation'
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: '사진을 등록해주세요.',
+  }),
+  title: z.string({
+    required_error: '제목을 입력해주세요.',
+  }),
+  price: z.coerce.number({
+    required_error: '가격을 입력해주세요.',
+  }),
+  description: z.string({
+    required_error: '설명을 입력해주세요.',
+  }),
+})
+
+export const uploadProduct = async (_: any, formData: FormData) => {
   const data = {
     photo: formData.get('photo'),
     title: formData.get('title'),
     price: formData.get('price'),
     description: formData.get('description'),
+  }
+
+  if (data.photo instanceof File) {
+    /**
+     * @description ArrayBuffer 객체는 일반적인 원시 바이너리 데이터 버퍼를 표현하는데 사용
+     * new ArrayBuffer(length)
+     * : 특정 바이트(Byte: 컴퓨터의 저장 단위, 컴퓨터가 조작하는 정보의 최소처리단위)크기(length)의 ArrayBuffer 객체 생성
+     */
+    const photoData = await data.photo.arrayBuffer()
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData))
+    data.photo = `/${data.photo.name}`
+  }
+
+  const result = productSchema.safeParse(data)
+
+  if (!result.success) {
+    return result.error.flatten()
+  } else {
+    const session = await getSession()
+
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      })
+      redirect(`/products/${product.id}`)
+    }
   }
 }
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -4,10 +4,12 @@ import Button from '@/components/button'
 import Input from '@/components/input'
 import { PhotoIcon } from '@heroicons/react/24/outline'
 import React, { useState } from 'react'
+import { useFormState } from 'react-dom'
 import { uploadProduct } from './actions'
 
 const AddProductsPage = () => {
   const [preview, setPreview] = useState('')
+  const [state, action] = useFormState(uploadProduct, null)
 
   const onChangeImage = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
@@ -43,7 +45,7 @@ const AddProductsPage = () => {
 
   return (
     <div>
-      <form className="flex flex-col gap-5 p-5" action={uploadProduct}>
+      <form className="flex flex-col gap-5 p-5" action={action}>
         <label
           htmlFor="photo"
           className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-300 bg-cover bg-center"
@@ -53,6 +55,7 @@ const AddProductsPage = () => {
             <>
               <PhotoIcon className="w-20" />
               <div>사진을 추가해주세요.</div>
+              {state?.fieldErrors.photo}
             </>
           )}
         </label>
@@ -64,9 +67,27 @@ const AddProductsPage = () => {
           className="hidden"
           onChange={onChangeImage}
         />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" required placeholder="가격" type="number" />
-        <Input name="description" required placeholder="상세설명" type="text" />
+        <Input
+          name="title"
+          required
+          placeholder="제목"
+          type="text"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          required
+          placeholder="가격"
+          type="number"
+          errors={state?.fieldErrors.price}
+        />
+        <Input
+          name="description"
+          required
+          placeholder="상세설명"
+          type="text"
+          errors={state?.fieldErrors.description}
+        />
         <Button text="작성완료" />
       </form>
     </div>
