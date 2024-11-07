8e1fcdf1281b5e4e2d057e8d013687aaac7c20a9
Manon Kim <kim.manon2@gmail.com>
2024-11-07 09:48:37 +0900

    [feat] dynamic page + suspense

--- app/(tab)/profile/page.tsx
+++ app/(tab)/profile/page.tsx
@@ -1,21 +1,31 @@
 import db from '@/lib/db'
 import getSession from '@/lib/session'
+import { notFound, redirect } from 'next/navigation'
+import { Suspense } from 'react'
 
-const ProfilePage = async () => {
-  const data = await getSession()
-  const user = await db.user.findUnique({
-    where: {
-      id: data.id,
-    },
-    select: {
-      username: true,
-      avatar: true,
-    },
-  })
+const getUser = async () => {
+  const session = await getSession()
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+      select: {
+        username: true,
+        avatar: true,
+      },
+    })
+    if (user) {
+      return user
+    }
+  }
+  notFound()
+}
 
+const Username = async () => {
+  const user = await getUser()
   return (
     <>
-      <div>ProfilePagee</div>
       <div>Hi {user?.username}</div>
       <img
         src={user?.avatar ?? ''}
@@ -23,6 +33,29 @@ const ProfilePage = async () => {
         width={200}
         alt="github-image"
       />
+    </>
+  )
+}
+
+// cookies를 사용하여 user마다 다른 내용을 보게되므로 dynamic페이지
+// suspense로 loading처리
+const ProfilePage = async () => {
+  const logOut = async () => {
+    'use server'
+    const session = await getSession()
+    await session.destroy()
+    redirect('/')
+  }
+
+  return (
+    <>
+      <div>ProfilePagee</div>
+      <Suspense fallback={'Welcome!'}>
+        <Username />
+      </Suspense>
+      <form action={logOut}>
+        <button>Log out</button>
+      </form>
     </>
   )
 }
