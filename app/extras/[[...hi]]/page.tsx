cf60e7c7b8c67d992dc8f443b884522f2ab7598f
Manon Kim <kim.manon2@gmail.com>
2024-11-21 23:17:00 +0900

    [feat] Catch all segments

--- app/extras/[[...hi]]/page.tsx
+++ app/extras/[[...hi]]/page.tsx
@@ -1,4 +1,5 @@
-const Extras = () => {
+const Extras = ({ params }: { params: { hi: string[] } }) => {
+  console.log(params)
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="font-bokor text-6xl">Extras!</h1>
