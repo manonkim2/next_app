7bb2912cfefbb930b226f09e25157a61ca219b8a
Manon Kim <kim.manon2@gmail.com>
2024-12-02 17:32:35 +0900

    [feat] logging

--- app/extras/[[...hi]]/page.tsx
+++ app/extras/[[...hi]]/page.tsx
@@ -1,10 +1,27 @@
-const Extras = ({ params }: { params: { hi: string[] } }) => {
-  console.log(params)
+import { revalidatePath } from 'next/cache'
+
+async function name() {
+  const data = await fetch(
+    'https://nomad-movies.nomadcoders.workers.dev/movies',
+  )
+}
+
+const Extras = async ({ params }: { params: { hi: string[] } }) => {
+  await name()
+
+  const action = async () => {
+    'use server'
+    revalidatePath('/extras')
+  }
+
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="font-bokor text-6xl">Extras!</h1>
       <h1 className="font-nanum text-6xl">Extras!</h1>
       <h2 className="font-releway">So much more to learn! we</h2>
+      <form action={action}>
+        <button>revalidate</button>
+      </form>
     </div>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 요청되는 request들을 시각화
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   // 이미지 최적화도 비용이기 때문에 최적화할 url과 아닌것을 구분
   images: {
     remotePatterns: [{ hostname: 'avatars.githubusercontent.com' }],
